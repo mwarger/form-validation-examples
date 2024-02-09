603448b7efe1de4c50402e7238ad481799f3b828
Mat Warger <686823+mwarger@users.noreply.github.com>
2024-02-09 12:14:08 -0700

    Initial commit

---
+++ src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="form-validation-examples" />
+    </div>
+  );
+}
+
+export default App;
